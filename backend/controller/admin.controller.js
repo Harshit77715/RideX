933edd435fe91d950abb20a1bd543c52671fc98b
Harshit <harshit619131@gmail.com>
2025-03-21 02:40:43 +0530

    Files Updated!

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -1,22 +1,15 @@
-import Booking from "../models/booking.model.js";
+import Booking from "../models/booking.model.js"
 
-const adminData = async (req, res) => {
+
+const adminData = async(req, res)=>{
     try {
-        const data = await Booking.find({})
-
-        res.status(200).json({
-            success: true,
-            message: "Bookings fetched successfully",
-            data
-        });
+       const data = await Booking.find({})
+       res.status(200).json(data); 
     } catch (error) {
-        console.error("Error fetching admin data:", error.message);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error",
-            error: error.message
-        });
+        console.log(error.message);
+        res.status(500).json({msg:"Internal server error"});
     }
 };
 
-export { adminData };
+
+export {adminData};
