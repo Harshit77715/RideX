67e4f65ec874a73646cb10c3fadcc5d72dc5ea65
Harshit <harshit619131@gmail.com>
2025-03-21 02:04:33 +0530

    updated admin

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -4,6 +4,18 @@ import Booking from "../models/booking.model.js"
 const adminData = async(req, res)=>{
     try {
        const data = await Booking.find({})
+       .populate({
+        path: "customerId", 
+        select: "name", 
+      })
+      .populate({
+        path: "driverId",
+        select: "name", 
+      })
+      .populate({
+        path: "carId", 
+        select: "model", 
+      });
        res.status(200).json(data); 
     } catch (error) {
         console.log(error.message);
